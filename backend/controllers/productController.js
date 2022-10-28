a2500524a704b84645b3d3e2bbcc1122d5c1f700
Chinu Anand <chinuanand81@gmail.com>
2022-10-28 10:53:30 +0530

    Creat getProducts function

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -10,12 +10,16 @@ exports.newProduct = async (req, res, next) => {
     });
 }
 
-// Get all Products => /api/v1/products
+// Display all Products => /api/v1/products
 
-exports.getProducts = (req, res, next) => {
+exports.getProducts = async (req, res, next) => {
+
+    const products = await Product.find();
+
     res.status(200).json({
         success: true,
-        message: "This route will show all products in database."
+        count: products.length,
+        data: products
     })
 }
 
