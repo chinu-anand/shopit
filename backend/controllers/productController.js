8c8f41e9ae4151ae873e2702c13329c45e610eeb
Chinu Anand <chinuanand81@gmail.com>
2022-11-07 12:00:24 +0530

    Built product filter API feature

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -17,7 +17,9 @@ exports.newProduct = catchAsyncError(async (req, res, next) => {
 // Display all Products => /api/v1/products?keyword=apple
 
 exports.getProducts = catchAsyncError(async (req, res, next) => {
-    const apiFeatures = new APIFeatures(Product.find(), req.query).search()
+    const apiFeatures = new APIFeatures(Product.find(), req.query)
+        .search()
+        .filter()
     const products = await apiFeatures.query;
 
     res.status(200).json({
--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -17,6 +17,26 @@ class APIFeatures {
         this.query = this.query.find({ ...keyword });
         return this;
     }
+
+    filter() {
+        const queryCopy = { ...this.queryStr };
+        // console.log(queryCopy);
+
+        // removing fields from query
+        const removeFields = ['keyword', 'limit', 'page'];
+
+        removeFields.forEach(el => delete queryCopy[el]);
+        // console.log(queryCopy);
+
+        // Advance filters for price, ratings etc.
+
+        let queryStr = JSON.stringify(queryCopy)
+
+        queryStr = queryStr.replace(/\b{gt|gte|lt|lte}\b/g, match => `$${match}`)
+
+        this.query = this.query.find(queryStr);
+        return this;
+    }
 }
 
 module.exports = APIFeatures;
