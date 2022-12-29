e6ea5a98b4f3031a0e20a14cb96f1a1ebe93e3b9
Chinu Anand <chinuanand81@gmail.com>
2022-12-29 11:44:48 +0530

    handle mongoose and jwt errors
    Signed-off-by: Chinu Anand <chinuanand81@gmail.com>

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -30,6 +30,24 @@ module.exports = (err, req, res, next) => {
             error = new ErrorHandler(message, 400);
         }
 
+        // handling mongoose duplicate errors
+        if (err.code === 11000) {
+            const message = `${Object.keys(err.keyValue)} already exists`;
+            error = new ErrorHandler(message, 400);
+        }
+
+        // handling wrong JWT error
+        if (err.name == 'JsonWebTokenError') {
+            const message = `JSON Web Token is invalid.`;
+            error = new ErrorHandler(message, 400);
+        }
+
+        // handling expired JWT error
+        if (err.name == 'TokenExpiredError') {
+            const message = `JSON Web Token is expired.`;
+            error = new ErrorHandler(message, 400);
+        }
+
         res.status(error.statusCode).json({
             success: false,
             message: error.message || 'Internal Server Error'
